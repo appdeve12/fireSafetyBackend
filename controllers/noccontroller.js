078479832458c7d3b435c8f37626c929723a59d4
appdeve12 <appdevelopergfo508@gmail.com>
2025-08-20 17:27:44 +0530

    update

--- controllers/noccontroller.js
+++ controllers/noccontroller.js
@@ -1,27 +1,42 @@
 const Noc=require("../models/NocUsers");
 const buyer=require("../models/user.buyer")
-exports.registerUserForNoc=async(req,res)=>{
-    try{
-        console.log("user")
-        const {name,mobile,state,city,size,areaUnit,purpose}=req.body;
-        console.log("name,mobile,state,city,size,areaUnit,purpose",name,mobile,state,city,size,areaUnit,purpose)
-        const BuyerId=req.user.id;
-        console.log(BuyerId)
-    
-        const nocuser= new Noc({
-            name,mobile,state,city,size,areaUnit,purpose,buyer:BuyerId
-        })
-await nocuser.save();
-buyer.nocRequests.push(nocuser._id);
-await buyer.save();
-
-res.status(201).json({
-    message:"user succedfully register for noc",
-    nocuser
-})
-    }catch(error){
-res.status(500).json({
-    message:"internal server error"
-})
+exports.registerUserForNoc = async (req, res) => {
+    try {
+      const { name, mobile, state, city, size, areaUnit, purpose } = req.body;
+      const BuyerId = req.user.id;
+  
+      // Create new NOC user
+      const nocuser = new Noc({
+        name,
+        mobile,
+        state,
+        city,
+        size,
+        areaUnit,
+        purpose,
+        buyer: BuyerId,
+      });
+  
+      await nocuser.save();
+  
+      // Find the buyer by ID
+      const buyerDoc = await buyer.findById(BuyerId);
+      if (!buyerDoc) {
+        return res.status(404).json({ message: "Buyer not found" });
+      }
+  
+      // Push noc request into buyer's array
+      buyerDoc.nocRequests.push(nocuser._id);
+      await buyerDoc.save();
+  
+      res.status(201).json({
+        message: "User successfully registered for NOC",
+        nocuser,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        message: "Internal server error",
+      });
     }
-}
+  };
